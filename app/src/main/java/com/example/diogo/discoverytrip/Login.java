9ff18795abee2b1a40e59ecf885401234d900dfb
Diogo Florêncio <diogo.lima@ccc.ufcg.edu.br>
2016-11-28 21:28:31 -0300

    Login e logout facebook funcionando

--- app/src/main/java/com/example/diogo/discoverytrip/Login.java
+++ app/src/main/java/com/example/diogo/discoverytrip/Login.java
@@ -3,10 +3,13 @@ package com.example.diogo.discoverytrip;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -15,10 +18,17 @@ public class Login extends AppCompatActivity {
     private LoginButton loginButton;
     private CallbackManager callbackManager;
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
+
+        textView = (TextView) findViewById(R.id.textView);
+
 
         loginButton = (LoginButton) findViewById(R.id.loginButton);
         callbackManager = CallbackManager.Factory.create();
@@ -26,17 +36,17 @@ public class Login extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+                textView.setText("Deu certo");
             }
 
             @Override
             public void onCancel() {
-
+                textView.setText("Foi cancelado");
             }
 
             @Override
             public void onError(FacebookException error) {
-
+                textView.setText("Deu erro");
             }
         });
 
