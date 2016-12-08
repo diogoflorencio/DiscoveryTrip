ea6d98477ad2cd3802ece07347a5ad5cd09eead8
dimitre <dimitreandrew@gmail.com>
2016-12-08 16:45:52 -0300

    login googleplus

--- app/src/main/java/com/example/diogo/discoverytrip/Login.java
+++ app/src/main/java/com/example/diogo/discoverytrip/Login.java
@@ -1,8 +1,12 @@
 package com.example.diogo.discoverytrip;
 
 import android.content.Intent;
+import android.content.IntentSender;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,14 +26,25 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.plus.Plus;
-import com.google.android.gms.plus.PlusOneButton;
-import com.google.android.gms.plus.PlusShare;
-import com.google.android.gms.plus.model.people.Person;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Status;
+//import com.google.android.gms.plus.Plus;
+//import com.google.android.gms.plus.PlusOneButton;
+//import com.google.android.gms.plus.PlusShare;
+//import com.google.android.gms.plus.model.people.Person;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 
 import java.util.Arrays;
 
-public class Login extends AppCompatActivity {
+import static com.example.diogo.discoverytrip.R.id.login_google;
+
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener, GoogleApiClient.ConnectionCallbacks {
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
@@ -37,6 +52,11 @@ public class Login extends AppCompatActivity {
     private ProfileTracker profileTracker;
 
     private TextView textView;
+
+    protected GoogleApiClient mGoogleApiClient;
+    private ConnectionResult connection_result;
+    private SignInButton login_google;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,10 +106,119 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.login_google).setOnClickListener(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.login_google);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
     }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        Login myApp = (Login )getApplicationContext();
+        myApp.mGoogleApiClient = mGoogleApiClient;
+    }
+
+    private void revokeAccess() {
+        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        updateUI(false);
+                    }
+                });
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Toast.makeText(this, String.valueOf(result.isSuccess()), Toast.LENGTH_SHORT).show();
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            startActivity(new Intent(Login.this,home.class));
+            //String personName = acct.getDisplayName();
+            //String personPhotoUrl = acct.getPhotoUrl().toString();
+            //String email = acct.getEmail();
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        //callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_google:
+                signIn();
+                break;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(this, "conectou", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "conexao suspensa", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "conexao falhou", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    private void updateUI(boolean isSignedIn) {
+        //caso queira alterar a UI
     }
 }
--- app/src/main/java/com/example/diogo/discoverytrip/home.java
+++ app/src/main/java/com/example/diogo/discoverytrip/home.java
@@ -14,10 +14,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -82,6 +86,9 @@ public class home extends AppCompatActivity
             return true;
         }
         if (id == R.id.logout){
+            //log out do google+
+            signOutGooglePlus();
+
             LoginManager.getInstance().logOut();
             Intent intent = new Intent(home.this,Login.class);
             startActivity(intent);
@@ -89,6 +96,27 @@ public class home extends AppCompatActivity
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void signOutGooglePlus() {
+        Toast.makeText(getApplicationContext(),"deslogando1",Toast.LENGTH_SHORT).show();
+        Login myApp = (Login )getApplicationContext();
+        Toast.makeText(getApplicationContext(),"deslogando2",Toast.LENGTH_SHORT).show();
+
+        if (myApp.mGoogleApiClient.isConnected()) {
+            Auth.GoogleSignInApi.signOut(myApp.mGoogleApiClient).setResultCallback(
+                    new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status status) {
+                            // [START_EXCLUDE]
+                            // [END_EXCLUDE]
+                        }
+                    });
+            //myApp.mGoogleApiClient.disconnect();
+            //myApp.mGoogleApiClient.connect();
+        }
+
+        Toast.makeText(getApplicationContext(),"deslogou",Toast.LENGTH_SHORT).show();
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
