53b5caa9eac94d856a7e0587de4179b6052ca107
Renato Ely <renato.silva@ccc.ufcg.edu.br>
2017-03-19 23:16:18 -0300

    Telas de detalhes de eventos e pontos turísticos

--- app/src/main/java/com/example/diogo/discoverytrip/Activities/DetalhesPontosTuristicosActivity.java
+++ app/src/main/java/com/example/diogo/discoverytrip/Activities/DetalhesPontosTuristicosActivity.java
@@ -1,28 +1,23 @@
 package com.example.diogo.discoverytrip.Activities;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.ViewGroup;
 
 import com.example.diogo.discoverytrip.Model.Atracao;
-import com.example.diogo.discoverytrip.Model.VisualizationType;
 import com.example.diogo.discoverytrip.R;
 
-public class DetalhesAtracaoActivity extends Activity {
-    public static Atracao atracao;
-    public static VisualizationType visualizationType;
-
+public class DetalhesPontosTuristicosActivity extends AppCompatActivity {
+    public static Atracao pontoTuristico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalhes_atracao);
+        setContentView(R.layout.activity_detalhes_pontos_turisticos);
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
-        atracao = null;
+        pontoTuristico = null;
     }
-
 }
